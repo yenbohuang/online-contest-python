b74f0db505c860a6ae5e25fd8ae132f541737960
Yenbo Huang <yenbo.huang@ctci.com>
2020-07-10 13:59:21 +0800

    easy: valid anagram

---
+++ leetcode/easy/test_valid_anagram.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/valid-anagram/
+
+import unittest
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        sLength = len(s)
+        tLength = len(t)
+
+        if sLength != tLength:
+            return False
+        
+        return sorted(s) == sorted(t)
+        
+class TestSolution(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+    
+    def tearDown(self):
+        pass
+
+    def test_case_1(self):
+        self.assertTrue(self.solution.isAnagram("abcd", "dcab"))
+    
+    def test_case_2(self):
+        self.assertTrue(self.solution.isAnagram("anagram", "nagaram"))
+    
+    def test_case_3(self):
+        self.assertFalse(self.solution.isAnagram("rat", "car"))
+
+if __name__ == '__main__':
+    unittest.main()
